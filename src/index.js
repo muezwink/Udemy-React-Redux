dbf4f38d6114c72815ac621d5515a1d9ab6493f6
muezwink <muezwink@gmail.com>
2024-01-11 03:10:32 +0900

    Shorthand JS Expressions

--- src/index.js
+++ src/index.js
@@ -10,9 +10,7 @@ const root = ReactDOM.createRoot(el);
 
 // 4) Create a component
 function App() {
-  const message = {};
-
-  return <h1>{message}</h1>;
+  return <h1>{new Date().toLocaleTimeString()}</h1>;
 }
 
 // 5) Show the component on the screen
