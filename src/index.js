c6e7c2c496e70b5135b37d07f16952430412f560
muezwink <muezwink@gmail.com>
2024-01-11 02:28:44 +0900

    Printing JavaScript Variables in JSX

--- src/index.js
+++ src/index.js
@@ -10,7 +10,9 @@ const root = ReactDOM.createRoot(el);
 
 // 4) Create a component
 function App() {
-  return <h1>Hi there!</h1>;
+  const message = {};
+
+  return <h1>{message}</h1>;
 }
 
 // 5) Show the component on the screen
