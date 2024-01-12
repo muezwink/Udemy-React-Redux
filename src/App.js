994b7d1aef32dd8b95295d65166c8ddb416d85cb
muezwink <muezwink@gmail.com>
2024-01-12 13:27:04 +0900

    Adding Custom CSS

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import './App.css';
 import { useState } from 'react';
 import AnimalShow from './AnimalShow';
 
@@ -15,11 +16,11 @@ function App() {
   };
 
   const renderedAnimals = animals.map((animal, index) => {
-    return <AnimalShow type={animal} key={index} />;
+    return <AnimalShow key={index} type={animal} />;
   });
 
   return (
-    <div>
+    <div className="app">
       <button onClick={handleClick}>Add Animal</button>
       <div>{renderedAnimals}</div>
     </div>
