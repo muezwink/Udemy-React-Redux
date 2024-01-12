7902bdc88517ad5de69802f7a32215e9e7ba8973
muezwink <muezwink@gmail.com>
2024-01-12 13:59:11 +0900

    Section 5:Using and API with React > Project Setup

--- src/App.js
+++ src/App.js
@@ -1,30 +1,5 @@
-import './App.css';
-import { useState } from 'react';
-import AnimalShow from './AnimalShow';
-
-function getRandomAnimal() {
-  const animals = ['bird', 'cat', 'cow', 'dog', 'gator', 'horse'];
-
-  return animals[Math.floor(Math.random() * animals.length)];
-}
-
 function App() {
-  const [animals, setAnimals] = useState([]);
-
-  const handleClick = () => {
-    setAnimals([...animals, getRandomAnimal()]);
-  };
-
-  const renderedAnimals = animals.map((animal, index) => {
-    return <AnimalShow key={index} type={animal} />;
-  });
-
-  return (
-    <div className="app">
-      <button onClick={handleClick}>Add Animal</button>
-      <div className="animal-list">{renderedAnimals}</div>
-    </div>
-  );
+  return <div>App</div>;
 }
 
 export default App;
