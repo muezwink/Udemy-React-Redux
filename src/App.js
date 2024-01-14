93dc93e01eec8a0f3e3e472d8fc2a292ba42039f
muezwink <muezwink@gmail.com>
2024-01-14 18:32:14 +0900

    State Location

--- src/App.js
+++ src/App.js
@@ -1,18 +1,9 @@
 import { useState } from 'react';
-import BookCreate from './components/BookCreate';
 
 function App() {
   const [books, setBooks] = useState([]);
 
-  const createBook = (title) => {
-    console.log('Need to add book with:', title);
-  };
-
-  return (
-    <div>
-      <BookCreate onCreate={createBook} />
-    </div>
-  );
+  return <div>App</div>;
 }
 
 export default App;
